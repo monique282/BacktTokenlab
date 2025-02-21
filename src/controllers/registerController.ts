254086c1a630601b6597f855841fe6431fe9b4b2
Monique de Souza <moniquejp16@gmail.com>
2025-02-21 11:37:29 -0300

    feat: creation of tables in schema.prisma

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+import { RegisterService } from "../service/registerService";
 
 async function registerPost(req: Request, res: Response) {
     const { name, password, cpf, email } = req.body;
-    const user = await RegisterService.registerPost({ name, password, cpf, email });
+    const user = await RegisterService.registerPost(name, password, cpf, email);
 
     res.status(httpStatus.CREATED).send(user);
 }
