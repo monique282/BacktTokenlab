898ce81e0d46b2313d33166fbe749eb49b0f4021
Monique de Souza <moniquejp16@gmail.com>
2025-02-21 13:06:12 -0300

    feat: creation liginService end sessionRepository

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
+import { LoginService } from "../service/loginService";
 
 async function loginPost(req: Request, res: Response) {
     const { mode, password } = req.body;
-    const user = await LoginService.loginPost({ mode, password });
+    const user = await LoginService.loginPost( mode, password );
 
     res.status(httpStatus.OK).send(user);
 }
---
+++ src/service/loginService.ts
@@ -0,0 +1,29 @@
+import * as jwt from "jsonwebtoken";
+import bcrypt from 'bcrypt';
+import { invalidCredentialsError } from "../erros/invalidCredentialsError";
+import { sessionRepository } from "../repositories/sessionRepository";
+
+async function loginPost(mode: string, password: string ) {
+    const login = await loginRepository.findByEmailPassword(mode);
+
+    if (!login) {
+        throw invalidCredentialsError("Email não cadastrado");
+    }
+
+    const passwordMatch = await bcrypt.compare(password, login.password);
+    if (!passwordMatch) {
+        throw invalidCredentialsError("Senha não corresposnde");
+    }
+
+    const token = jwt.sign({ userId: login.id, email: login.email }, process.env.JWT_SECRET, { expiresIn: '2d' });
+    const session = await sessionRepository.sessionToken(token, login.id);
+    const list = {
+        email: login.email,
+        name: login.name,
+    };
+    return [list, session];
+}
+
+export const LoginService = {
+    loginPost
+};
