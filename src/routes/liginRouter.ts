5da5741b18d3f0e6c63edfaa042311ae446258f2
Monique de Souza <moniquejp16@gmail.com>
2025-02-21 12:58:40 -0300

    feat: creation liginRouter

--- src/routes/liginRouter.ts
+++ src/routes/liginRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { ObjectSchema } from 'joi';
 import { validateBody } from '../middlewares/validationMiddlewere';
 import { LoginSchema } from '../schemas/loginSchemars';
+import { loginController } from '../controllers/loginController';
 
 
 const UserRouter = Router();
