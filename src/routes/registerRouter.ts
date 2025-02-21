d735f50a9ca219fd5d93bbf4c6f1da103995935e
Monique de Souza <moniquejp16@gmail.com>
2025-02-21 11:24:06 -0300

    feat: criation RegisterController

--- src/routes/registerRouter.ts
+++ src/routes/registerRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { ObjectSchema } from 'joi';
 import { RegisterSchema } from '../schemas/registerSchemas';
 import { validateBody } from '../middlewares/validationMiddlewere';
+import { registerController } from '../controllers/registerController';
 
 
 const RegisterRouter = Router();
